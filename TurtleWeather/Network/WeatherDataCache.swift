945cd563d6fa673ae05beecdc05cca91ad80e6fc
TimB <tim@skilpaddelabs.com>
2016-04-21 10:40:02 -0400

    Created folder/group for Network classes. Added WeatherDataCache class.

---
+++ TurtleWeather/Network/WeatherDataCache.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherDataCache.swift
+//  TurtleWeather
+//
+//  Created by Tim Bolstad on 4/21/16.
+//  Copyright © 2016 Skilpadde Labs. All rights reserved.
+//
+
+import Foundation
+
+class WeatherDataCache {
+    
+}
